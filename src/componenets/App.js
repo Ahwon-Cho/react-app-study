d53d3f0181a7d1deae2e50d81f5e646bc37856e7
Ahwon Cho <awony82@gmail.com>
2020-11-13 09:48:59 -0800

    change

---
+++ src/componenets/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './SearchBar'
+
+const App = () => {
+    return (
+        <div className="ui container" style={{marginTop: '10px'}}>
+            <SearchBar />
+            Hello!
+            </div>
+    )
+}
+
+export default App;

d685542d251983692645ed3a709152c109e07108
Ahwon Cho <awony82@gmail.com>
2021-01-20 14:32:01 -0800

    deleted api key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
 export default axios.create ({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 
-        'Client-ID AyOMqJkB3txQq5ZAaVCLgLIoK-NruqUwO-cPPSf8hy8'
+
     }
 });
